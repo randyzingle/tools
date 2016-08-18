9b424e355ac93f195960132aeee860df4215e9d3
Randy Zingle <randall.zingle@sas.com>
2016-08-18 09:53:34 -0400

    kafka consumer linked to specific partition

--- kafka-client/src/main/java/com/sas/mkt/kafka/clients/MessageConsumer.java
+++ kafka-client/src/main/java/com/sas/mkt/kafka/clients/MessageConsumer.java
@@ -24,33 +24,58 @@ public class MessageConsumer {
 	     props.put("session.timeout.ms", "30000");
 
 		MessageConsumer mc = new MessageConsumer();
-		mc.getMessages();
+//		mc.getMessages();
+		mc.getMessagesFromPartition();
 
+	}
+
+	private void getMessagesFromPartition() {
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "kafka0.cidev.sas.us:9092");
+		props.put("group.id", "baldur22");
+		props.put("enable.auto.commit", "true");
+		props.put("auto.commit.interval.ms", "1000");
+		props.put("session.timeout.ms", "30000");
+		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+		// Subscribe to a particular topic AND partition
+		// use assign vs subscribe (you can not mix the two)
+		TopicPartition tp = new TopicPartition(topic, 0);
+		consumer.assign(Arrays.asList(tp));
+		consumer.seek(tp, 0);
+		System.out.println("assigned consumer to partition: " + tp.toString());
+		while (true) {
+			ConsumerRecords<String, String> records = consumer.poll(100);
+			for (ConsumerRecord<String, String> record : records) {
+				System.out.printf("offset = %d, key = %s, value = %s", record.offset(), record.key(), record.value());
+				System.out.println("");
+			}
+		}
 	}
 
 	private void getMessages() {
 		Properties props = new Properties();
-	     props.put("bootstrap.servers", "kafka0.cidev.sas.us:9092");
-	     props.put("group.id", "baldurxxx");
-	     props.put("enable.auto.commit", "true");
-	     props.put("auto.commit.interval.ms", "1000");
-	     props.put("session.timeout.ms", "30000");
-	     props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-	     props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-	     KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-	     consumer.subscribe(Arrays.asList(topic));
-	     System.out.println("subscribed to topics...");
-	     TopicPartition tp = new TopicPartition(topic, 1);
-//	     consumer.seek(tp, 0);
-	     while (true) {
-	         ConsumerRecords<String, String> records = consumer.poll(100);
-	         for (ConsumerRecord<String, String> record : records) {
-	             System.out.printf("offset = %d, key = %s, value = %s", record.offset(), record.key(), record.value());
-	             System.out.println("");
-	         }
-	         
-	     }
-	 
+		props.put("bootstrap.servers", "kafka0.cidev.sas.us:9092");
+		props.put("group.id", "baldurxxx");
+		props.put("enable.auto.commit", "true");
+		props.put("auto.commit.interval.ms", "1000");
+		props.put("session.timeout.ms", "30000");
+		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+		// subscribe to the topic - subscribe will let Kafka dynamically assign partitions to the consumers in the group
+		consumer.subscribe(Arrays.asList(topic));
+		System.out.println("subscribed to topics...");
+
+		while (true) {
+			ConsumerRecords<String, String> records = consumer.poll(100);
+			for (ConsumerRecord<String, String> record : records) {
+				System.out.printf("offset = %d, key = %s, value = %s", record.offset(), record.key(), record.value());
+				System.out.println("");
+			}
+		}
+
 	}
 
 }

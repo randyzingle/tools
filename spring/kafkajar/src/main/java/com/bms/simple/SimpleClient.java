8c1eb5e1ca692e7e53ecca19a3bb8f00593f859f
Randy Zingle <randall.zingle@sas.com>
2017-03-03 16:53:34 -0500

    jar file project

---
+++ spring/kafkajar/src/main/java/com/bms/simple/SimpleClient.java
@@ -0,0 +1,15 @@
+package com.bms.simple;
+
+public class SimpleClient {
+
+	private String getMessage;
+
+	public String getGetMessage() {
+		return getMessage;
+	}
+
+	public void setGetMessage(String getMessage) {
+		this.getMessage = getMessage;
+	}
+	
+}

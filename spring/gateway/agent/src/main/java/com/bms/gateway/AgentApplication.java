ca6e6b5ea842eda44abcbe45cce1446f3eef945c
Randy Zingle <randy.zingle@gmail.com>
2017-07-03 15:51:00 -0400

    take out aws and config libs for now

--- spring/gateway/agent/src/main/java/com/bms/gateway/AgentApplication.java
+++ spring/gateway/agent/src/main/java/com/bms/gateway/AgentApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GatewayApplication {
+public class AgentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
+		SpringApplication.run(AgentApplication.class, args);
 	}
 }

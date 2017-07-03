cff3e5d7e03b3ec194eb3d7084845c491296b938
Randy Zingle <randy.zingle@gmail.com>
2017-07-03 17:41:41 -0400

    simple controller

--- spring/gateway/server/src/main/java/com/bms/gateway/ServerApplication.java
+++ spring/gateway/server/src/main/java/com/bms/gateway/ServerApplication.java
@@ -3,10 +3,16 @@ package com.bms.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SpringBootApplication
 public class ServerApplication {
+	
+	private static final Logger log = LoggerFactory.getLogger(ServerApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
+		log.info("ServerApplication started");
 	}
 }

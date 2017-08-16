6a2b12ca9fdfb467433699c856092cad8cd0eb4f
Randy Zingle <randy.zingle@gmail.com>
2017-08-16 12:51:15 -0400

    first

---
+++ spring/demo/src/main/java/com/bms/demo/domain/Greeting.java
@@ -0,0 +1,26 @@
+package com.bms.demo.domain;
+
+public class Greeting {
+	
+	private long id;
+	private String message;
+	
+	public Greeting(long id, String message) {
+		this.id = id;
+		this.message = message;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
